65966fdebcf1fb07f9a6de7eeef755307ababa1e
Krystopher Quintero <krystopher.q@gmail.com>
2022-05-02 15:50:04 -0700

    create db and tables

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
+  id INT AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL() NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES (department(id))
+);
+
+CREATE TABLE employee(
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  FOREIGN KEY(roles_id)
+  REFERENCES(roles(id)),
+  FOREIGN KEY (manager_id)
+  REFERENCES (employee(id)),
+  PRIMARY KEY(id),
+);
+
