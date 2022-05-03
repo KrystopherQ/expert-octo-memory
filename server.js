64a227d12b303751a549d0d1d0be3c1263672219
Krystopher Quintero <krystopher.q@gmail.com>
2022-05-02 17:17:03 -0700

    add inquierer prompts

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
-
+const inquirer = require('inquirer')
+const cTable = require('console.table')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,14 +12,166 @@ const db = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'password',
-        database: ''
+        database: 'company_db'
     },
     console.log(``)
 );
 
+function begin() {
+    inquirer.prompt({
+            name: 'start',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department'
+            ]
+        })
+        .then((selected) => {
+            switch (selected.begin) {
+                case 'View All Employees':
+                    viewAllEmployees()
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployee();
+                    break;
+                case 'View All Roles':
+                    viewAllRoles()
+                    break;
+                case 'Add Role':
+                    addRole()
+                    break;
+                case 'View All Departments':
+                    viewAllDepartments()
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+            }
+        })
+}
 
+const viewAllEmployees = () => {
+    db.query()
+}
+const addEmployee = () => {
+    inquirer.prompt([{
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        }
+    ])
+}
 
+const updateEmployee = () => {
+    inquirer.prompt([{
+            type: 'input',
+            message: "",
+            name: '',
 
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+
+    ])
+}
+
+const viewAllRoles = () => {
+    db.query()
+}
+
+const addRole = () => {
+    inquirer.prompt([{
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        }
+
+    ])
+
+}
+
+const viewAllDepartments = () => {
+    db.query()
+}
+
+const addDepartment = () => {
+    inquirer.prompt([{
+            type: 'input',
+            message: "",
+            name: '',
+
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+        {
+            type: 'input',
+            message: "",
+            name: '',
+        },
+
+    ])
+
+}
 
 
 
